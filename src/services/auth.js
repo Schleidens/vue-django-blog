cf520dd677c869b3c1434fcef026d909e2f2a19d
Schleidens <alectineschleidens2016@gmail.com>
2023-06-12 15:39:20 -0400

    set function for login and set token receive from api to localStoreage

---
+++ src/services/auth.js
@@ -0,0 +1,16 @@
+import ApiService from "./ApiService";
+
+// function to log in obtain a token
+export async function login(username, password){
+    try {
+        const response = await ApiService.post('login/', { username, password });
+        const token = response.data.token
+        //set token in localStorage
+        localStorage.setItem("token", token);
+        //set the default Authorization header
+        ApiService.defaults.headers.common['Authorization'] = `Token ${token}`
+        return true;
+    } catch (error) {
+        console.log("Login failed", error)
+    }
+}
