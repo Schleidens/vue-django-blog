682628ff59ad3890dd0ecd6f92196b1fd249cfe5
Schleidens <alectineschleidens2016@gmail.com>
2023-06-20 13:36:40 -0400

    create 404 page and route

---
+++ src/views/NotFoundPageView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-center">
+            <h1 class="fw-bold">
+                <span>4</span>&#128579;<span>4</span>
+            </h1>
+            <h2>
+                Page not found
+            </h2>
+            <RouterLink to="/">
+                Back to home
+            </RouterLink>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
