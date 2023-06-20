5fd6f34767caa60ce57f4ad966ccb62e6fdba90b
Schleidens <alectineschleidens2016@gmail.com>
2023-06-20 13:48:57 -0400

    create loading animation component

---
+++ src/components/LoadingAnimation.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="loading-dots">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+</template>
